3495aadcef1b4a0276f4cef3f3c109d68fabe099
Adam Christie <fractos@gmail.com>
2019-10-02 16:13:43 +0100

    remove unneeded count from cloudwatch_event_target.

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -1,5 +1,4 @@
 resource "aws_cloudwatch_event_target" "ecs_ratatoskr" {
-  count     = "${length(var.source_package) > 0 ? 1 : 0}"
   target_id = "${var.prefix}-ecs-ratatoskr"
   rule      = "${aws_cloudwatch_event_rule.ecs.name}"
   arn       = "${local.lambda_function_arn}"
