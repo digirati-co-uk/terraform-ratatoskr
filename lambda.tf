726e37845fbc2ab6355dacf0f9d244d0057a7905
Donald Gray <donald.gray@digirati.com>
2021-12-10 10:27:07 +0000

    Update syntax for tf 1.0

--- lambda.tf
+++ lambda.tf
@@ -1,56 +1,56 @@
 resource "aws_lambda_function" "ratatoskr_file" {
-  count            = "${length(var.source_package) > 0 ? 1 : 0}"
+  count            = length(var.source_package) > 0 ? 1 : 0
   function_name    = "${var.prefix}-ratatoskr"
   handler          = "main.lambda_handler"
   runtime          = "python3.9"
-  filename         = "${var.source_package}"
-  source_code_hash = "${base64sha256(file(var.source_package))}"
+  filename         = var.source_package
+  source_code_hash = base64sha256(file(var.source_package))
 
-  role = "${aws_iam_role.ratatoskr_exec_role.arn}"
+  role = aws_iam_role.ratatoskr_exec_role.arn
 
   environment {
     variables = {
-      SLACK_API_TOKEN                = "${var.slack_api_token}"
-      SLACK_CHANNEL                  = "${var.slack_channel}"
-      INCLUDED_CLUSTERS              = "${var.included_clusters}"
-      TABLE_TASK_STATE               = "${aws_dynamodb_table.ratatoskr_task_state.name}"
-      TABLE_TASK_DIGEST              = "${aws_dynamodb_table.ratatoskr_task_digest.name}"
-      TABLE_CONTAINER_INSTANCE_STATE = "${aws_dynamodb_table.ratatoskr_instance_state.name}"
+      SLACK_API_TOKEN                = var.slack_api_token
+      SLACK_CHANNEL                  = var.slack_channel
+      INCLUDED_CLUSTERS              = var.included_clusters
+      TABLE_TASK_STATE               = aws_dynamodb_table.ratatoskr_task_state.name
+      TABLE_TASK_DIGEST              = aws_dynamodb_table.ratatoskr_task_digest.name
+      TABLE_CONTAINER_INSTANCE_STATE = aws_dynamodb_table.ratatoskr_instance_state.name
     }
   }
 }
 
 resource "aws_lambda_function" "ratatoskr_s3" {
-  count         = "${length(var.source_package) > 0 ? 0 : 1}"
+  count         = length(var.source_package) > 0 ? 0 : 1
   function_name = "${var.prefix}-ratatoskr"
   handler       = "main.lambda_handler"
   runtime       = "python3.9"
-  s3_bucket     = "${var.source_s3_bucket}"
-  s3_key        = "${var.source_s3_key}"
+  s3_bucket     = var.source_s3_bucket
+  s3_key        = var.source_s3_key
 
-  role = "${aws_iam_role.ratatoskr_exec_role.arn}"
+  role = aws_iam_role.ratatoskr_exec_role.arn
 
   environment {
     variables = {
-      SLACK_API_TOKEN                = "${var.slack_api_token}"
-      SLACK_CHANNEL                  = "${var.slack_channel}"
-      INCLUDED_CLUSTERS              = "${var.included_clusters}"
-      TABLE_TASK_STATE               = "${aws_dynamodb_table.ratatoskr_task_state.name}"
-      TABLE_TASK_DIGEST              = "${aws_dynamodb_table.ratatoskr_task_digest.name}"
-      TABLE_CONTAINER_INSTANCE_STATE = "${aws_dynamodb_table.ratatoskr_instance_state.name}"
+      SLACK_API_TOKEN                = var.slack_api_token
+      SLACK_CHANNEL                  = var.slack_channel
+      INCLUDED_CLUSTERS              = var.included_clusters
+      TABLE_TASK_STATE               = aws_dynamodb_table.ratatoskr_task_state.name
+      TABLE_TASK_DIGEST              = aws_dynamodb_table.ratatoskr_task_digest.name
+      TABLE_CONTAINER_INSTANCE_STATE = aws_dynamodb_table.ratatoskr_instance_state.name
     }
   }
 }
 
 locals {
-  lambda_function_name = "${element(concat(aws_lambda_function.ratatoskr_file.*.function_name, aws_lambda_function.ratatoskr_s3.*.function_name), 0)}"
-  lambda_function_arn  = "${element(concat(aws_lambda_function.ratatoskr_file.*.arn, aws_lambda_function.ratatoskr_s3.*.arn), 0)}"
+  lambda_function_name = element(concat(aws_lambda_function.ratatoskr_file.*.function_name, aws_lambda_function.ratatoskr_s3.*.function_name), 0)
+  lambda_function_arn  = element(concat(aws_lambda_function.ratatoskr_file.*.arn, aws_lambda_function.ratatoskr_s3.*.arn), 0)
 }
 
 resource "aws_lambda_permission" "allow_cloudwatch_to_call_ratatoskr" {
   statement_id  = "AllowExecutionFromCloudwatch"
   action        = "lambda:InvokeFunction"
-  function_name = "${local.lambda_function_name}"
+  function_name = local.lambda_function_name
   principal     = "events.amazonaws.com"
-  source_arn    = "${aws_cloudwatch_event_rule.ecs.arn}"
+  source_arn    = aws_cloudwatch_event_rule.ecs.arn
 }
